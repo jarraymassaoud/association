4e98453419314105baaacd8023b6d817ba74be93
jarray massaoud <jarraymassoud@gmail.com>
2026-08-10 11:36:04 +0100

    pages 404

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom"; // 👈 زدنا هذا
+import { HashRouter } from "react-router-dom"; // 👈 زدنا هذا
 import Main from "./components/Main";
 import { LanguageProvider } from "./context/LanguageContext";
 import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <LanguageProvider>
         <Main />
       </LanguageProvider>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -7,16 +7,13 @@ import drapeau from "../assets/images/Flag_of_Tunisia.gif";
 const Header = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
   const toggleNav = () => setIsNavOpen(!isNavOpen);
-
-  // دالة لإغلاق القائمة في التلفون عند النقر على أي رابط
   const closeNav = () => setIsNavOpen(false);
 
   return (
     <React.Fragment>
-      {/* الشريط العلوي متجاوب لجميع الشاشات */}
+      {/* الشريط العلوي */}
       <div className="container my-2" style={{ direction: "rtl" }}>
         <div className="row align-items-center justify-content-between">
-          {/* اللوجو والاسم - يأخذ المساحة الكاملة المتاحة */}
           <div className="col-9 col-md-8 d-flex align-items-center">
             <img
               src={logo1}
@@ -30,7 +27,6 @@ const Header = () => {
             </span>
           </div>
 
-          {/* علم تونس على اليسار */}
           <div className="col-3 col-md-4 d-flex justify-content-end align-items-center">
             <img
               src={drapeau}
@@ -41,10 +37,15 @@ const Header = () => {
         </div>
       </div>
 
-      {/* القائمة الرئيسية مرتبة ومتجاوبة */}
-      <Navbar dark expand="md" style={{ direction: "rtl" }}>
+      {/* 🟢 القائمة الرئيسية بخلفية فاتحة ولون خط أسود (Noir) */}
+      <Navbar
+        light
+        expand="md"
+        className="bg-light border-top border-bottom shadow-sm"
+        style={{ direction: "rtl", zIndex: 9999, position: "relative" }}
+      >
         <div className="container">
-          {/* زر التلفون (Hamburger Button) */}
+          {/* زر الموبايل */}
           <NavbarToggler onClick={toggleNav} className="ms-auto" />
 
           <Collapse isOpen={isNavOpen} navbar>
@@ -52,30 +53,59 @@ const Header = () => {
               navbar
               className="w-100 d-flex justify-content-start pr-0 py-2 py-md-0"
             >
-              <NavItem className="ml-md-4 my-1 my-md-0">
-                <NavLink className="nav-link" to="/home" onClick={closeNav}>
-                  <span className="fa fa-home fa-lg ml-1"></span> الرئيسية
+              <NavItem className="ml-md-4 my-2 my-md-0">
+                <NavLink
+                  className="nav-link text-dark fw-bold"
+                  style={{ color: "#000000" }}
+                  to="/home"
+                  onClick={closeNav}
+                >
+                  <span className="fa fa-home fa-lg ml-1 text-dark"></span>{" "}
+                  الرئيسية
                 </NavLink>
               </NavItem>
-              <NavItem className="ml-md-4 my-1 my-md-0">
-                <NavLink className="nav-link" to="/about" onClick={closeNav}>
-                  <span className="fa fa-info fa-lg ml-1"></span> حول الجمعية
+              <NavItem className="ml-md-4 my-2 my-md-0">
+                <NavLink
+                  className="nav-link text-dark fw-bold"
+                  style={{ color: "#000000" }}
+                  to="/about"
+                  onClick={closeNav}
+                >
+                  <span className="fa fa-info fa-lg ml-1 text-dark"></span> حول
+                  الجمعية
                 </NavLink>
               </NavItem>
-              <NavItem className="ml-md-4 my-1 my-md-0">
-                <NavLink className="nav-link" to="/service" onClick={closeNav}>
-                  <span className="fa fa-list fa-lg ml-1"></span> الخدمات
+              <NavItem className="ml-md-4 my-2 my-md-0">
+                <NavLink
+                  className="nav-link text-dark fw-bold"
+                  style={{ color: "#000000" }}
+                  to="/service"
+                  onClick={closeNav}
+                >
+                  <span className="fa fa-list fa-lg ml-1 text-dark"></span>{" "}
+                  الخدمات
                 </NavLink>
               </NavItem>
-              <NavItem className="ml-md-4 my-1 my-md-0">
-                <NavLink className="nav-link" to="/download" onClick={closeNav}>
-                  <span className="fa fa-download fa-lg ml-1"></span> التنزيلات
+              <NavItem className="ml-md-4 my-2 my-md-0">
+                <NavLink
+                  className="nav-link text-dark fw-bold"
+                  style={{ color: "#000000" }}
+                  to="/download"
+                  onClick={closeNav}
+                >
+                  <span className="fa fa-download fa-lg ml-1 text-dark"></span>{" "}
+                  التنزيلات
                 </NavLink>
               </NavItem>
-              <NavItem className="ml-md-4 my-1 my-md-0">
-                <NavLink className="nav-link" to="/contact" onClick={closeNav}>
-                  <span className="fa fa-address-card fa-lg ml-1"></span> اتصل
-                  بنا
+              <NavItem className="ml-md-4 my-2 my-md-0">
+                <NavLink
+                  className="nav-link text-dark fw-bold"
+                  style={{ color: "#000000" }}
+                  to="/contact"
+                  onClick={closeNav}
+                >
+                  <span className="fa fa-address-card fa-lg ml-1 text-dark"></span>{" "}
+                  اتصل بنا
                 </NavLink>
               </NavItem>
             </Nav>
